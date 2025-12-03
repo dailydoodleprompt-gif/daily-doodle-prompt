d987698080e021e03a41726fdab24bacd2ff5b80
dailydoodleprompt-gif <dailydoodleprompt@gmail.com>
2025-12-03 01:08:20 -0500

    Create mcp-server.ts

---
+++ src/sdk/constants/mcp-server.ts
@@ -0,0 +1,30 @@
+export type McpServerId = keyof typeof MCP_SERVERS;
+
+// { "mcp-id": { id: string, name: string, url: string } }
+export const MCP_SERVERS = {
+  "686de48c6fd1cae1afbb55ba": {
+    "id": "686de48c6fd1cae1afbb55ba",
+    "name": "GoogleSheets",
+    "url": "https://backend.composio.dev/v3/mcp/01b46296-8ea1-4686-a211-87ba79800a99/mcp?user_id=692f994099579dee36d6ebeb"
+  },
+  "688338e4ee9e1a9340d83b62": {
+    "id": "688338e4ee9e1a9340d83b62",
+    "name": "Slack",
+    "url": "https://backend.composio.dev/v3/mcp/8ef98e31-cf09-45a1-b230-e9b01571a628/mcp?user_id=692f994099579dee36d6ebeb"
+  },
+  "686de4616fd1cae1afbb55b9": {
+    "id": "686de4616fd1cae1afbb55b9",
+    "name": "Notion",
+    "url": "https://backend.composio.dev/v3/mcp/4c5f76f7-bbb8-4a8a-a478-ab5a2d7b0fa2/mcp?user_id=692f994099579dee36d6ebeb"
+  },
+  "686de4e26fd1cae1afbb55bc": {
+    "id": "686de4e26fd1cae1afbb55bc",
+    "name": "Github",
+    "url": "https://backend.composio.dev/v3/mcp/4e4720cb-01e7-4e14-88f8-3567f0453182/mcp?user_id=692f994099579dee36d6ebeb"
+  },
+  "686de4b46fd1cae1afbb55bb": {
+    "id": "686de4b46fd1cae1afbb55bb",
+    "name": "Discord",
+    "url": "https://backend.composio.dev/v3/mcp/9f018a18-b585-4c64-b9a9-4a286a41e43a/mcp?user_id=692f994099579dee36d6ebeb"
+  }
+};
