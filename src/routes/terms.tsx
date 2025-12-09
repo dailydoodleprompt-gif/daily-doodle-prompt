993602422c9a0bdd758b7d3bf5ae1aa969c9b21a
dailydoodleprompt-gif <dailydoodleprompt@gmail.com>
2025-12-08 22:39:47 -0500

    Delete src/routes/terms.tsx

--- src/routes/terms.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { TermsOfServiceView } from '@/views/TermsOfServiceView';
-
-export const Route = createFileRoute()({
-  component: () => <TermsOfServiceView onBack={() => window.history.back()} />,
-});
