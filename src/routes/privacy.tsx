ab65d7144010bfa2c6eab54a2278ee2b42134aab
dailydoodleprompt-gif <dailydoodleprompt@gmail.com>
2025-12-08 22:39:39 -0500

    Delete src/routes/privacy.tsx

--- src/routes/privacy.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { PrivacyPolicyView } from '@/views/PrivacyPolicyView';
-
-export const Route = createFileRoute()({
-  component: () => <PrivacyPolicyView onBack={() => window.history.back()} />,
-});
