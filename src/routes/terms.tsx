2538753836235cf00e68b7f6a62d835674227935
dailydoodleprompt-gif <dailydoodleprompt@gmail.com>
2025-12-17 13:20:59 -0800

    Wire Terms route through index to restore utility back navigation

--- src/routes/terms.tsx
+++ src/routes/terms.tsx
@@ -1,7 +1,4 @@
 // FILE: src/routes/terms.tsx
 import { createFileRoute } from '@tanstack/react-router';
-import { TermsOfServiceView } from '@/views/TermsOfServiceView';
 
-export const Route = createFileRoute('/')({
-  component: TermsOfServiceView,
-});
+export const Route = createFileRoute('/terms')({});
