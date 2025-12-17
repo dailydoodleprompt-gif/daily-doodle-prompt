04a3dee76dbb661a57179354440d2783003ffb7b
dailydoodleprompt-gif <dailydoodleprompt@gmail.com>
2025-12-17 13:24:07 -0800

    Fix terms route to match file-based routing pattern

--- src/routes/terms.tsx
+++ src/routes/terms.tsx
@@ -1,4 +1,4 @@
 // FILE: src/routes/terms.tsx
 import { createFileRoute } from '@tanstack/react-router';
 
-export const Route = createFileRoute('/terms')({});
+export const Route = createFileRoute('/')({});
