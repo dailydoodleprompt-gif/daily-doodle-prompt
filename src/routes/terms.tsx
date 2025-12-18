226674010271df7790d42761b1a495e182a65019
dailydoodleprompt-gif <dailydoodleprompt@gmail.com>
2025-12-17 17:47:21 -0800

    undo

--- src/routes/terms.tsx
+++ src/routes/terms.tsx
@@ -2,16 +2,3 @@
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({});
-
-
-// FILE: src/routes/terms.tsx
-import { createFileRoute } from '@tanstack/react-router';
-import { TermsOfServiceView } from '@/views/TermsOfServiceView';
-
-export const Route = createFileRoute('/')({
-  component: PrivacyRoute,
-});
-
-function TermsRoute() {
-  return <TermsofServiceView onBack={() => window.history.back()} />;
-}
